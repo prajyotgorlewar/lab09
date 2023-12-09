c9ec87abf210f548d690c65c321058efca9aa7e0
prajyot gorlewar <prajyotgorlewargmail.com>
2023-12-09 23:33:49 +0530

    third commit

---
+++ app/Http/Controllers/AdminUsersController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\AddUserRequest;
+use App\Http\Requests\EditUserRequest;
+use App\Models\Role;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Session;
+
+class AdminUsersController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $users = User::all();
+
+        return view('admin.users.index', compact(['users']));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        $roles = Role::all();
+        return view('admin.users.create', compact('roles'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(AddUserRequest $request)
+    {
+        $user = $request->all();
+        $user['password'] = Hash::make('Admin123');
+        User::create($user);
+        Session::flash('admin_flash', 'User created successfully.');
+        return redirect(route('admin-users'));
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        $user = User::find($id);
+        if( $user == null) {
+            Session::flash('admin_flash', 'User does not exist.');
+            return redirect(route('admin-users'));
+        }
+        $roles = Role::all();
+        return view('admin.users.edit', compact('roles', 'user'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(EditUserRequest $request, string $id)
+    {
+        $user = User::find($id);
+        $input = $request->all();
+        $user->update($input);
+        Session::flash('admin_flash', 'User edited successfully.');
+        return redirect(route('admin-users'));
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
---
+++ app/Http/Requests/AddUserRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class AddUserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => ['required','max:100'],
+            'email' => ['required',Rule::unique('users')],
+            'role_id' => ['required'],
+            'active' => ['required']
+        ];
+    }
+}
